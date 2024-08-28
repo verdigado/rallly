5d198dad29c571332cc9148a81629e3e83e02847
Niko Heller <hellerniko@gmail.com>
2024-08-28 14:38:57 +0200

    29: add termite alert on start and login page

---
+++ apps/web/src/app/[locale]/(admin)/termite-alert.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Alert, AlertDescription, AlertTitle } from "@rallly/ui/alert";
+import { Button } from "@rallly/ui/button";
+import { InfoIcon } from "lucide-react";
+import { useState } from "react";
+
+export function TermiteAlert() {
+  const [showTermiteAlert, setShowTermiteAlert] = useState(true);
+
+  if (!showTermiteAlert) {
+    return null;
+  }
+
+  return (
+    <Alert className="my-4" icon={InfoIcon}>
+      <AlertTitle>Herzlich Willkommen bei der neuen Termite! 🎉</AlertTitle>
+      <AlertDescription>
+        <p className="mt-4">
+          Zum 02.09.2024 wurde die Termite aktualisiert. Eine Anleitung zur
+          Nutzung der Termite 2.0 findet ihr{" "}
+          <a
+            className="text-link"
+            href="https://netz.gruene.de/de/wissenswerk/2024-08/die-neue-termite"
+            target="_blank"
+          >
+            hier
+          </a>
+          .
+        </p>
+        <p className="mt-2">
+          Bei Fragen oder Feedback wendet euch gerne an <strong>beteiligung@gruene.de</strong>.
+        </p>
+        <div>
+          <Button
+            className="mt-4"
+            variant="primary"
+            onClick={() => setShowTermiteAlert(false)}
+          >
+            Ausblenden
+          </Button>
+        </div>
+      </AlertDescription>
+    </Alert>
+  );
+}
