b6bd8885d414afe54ce1c4761ee9a50b75f83dd8
DunEl <liss.dunphy@verdigado.com>
2024-08-29 21:51:41 +0200

    29: customize styling

--- apps/web/src/app/[locale]/(admin)/termite-alert.tsx
+++ apps/web/src/app/[locale]/(admin)/termite-alert.tsx
@@ -13,7 +13,7 @@ export function TermiteAlert() {
   }
 
   return (
-    <Alert className="my-4" icon={InfoIcon}>
+    <Alert className="my-4 termite-alert" icon={InfoIcon}>
       <AlertTitle>Herzlich Willkommen bei der neuen Termite! 🎉</AlertTitle>
       <AlertDescription>
         <p className="mt-4">
