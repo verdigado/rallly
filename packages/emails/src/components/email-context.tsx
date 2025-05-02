29c51ac0b2bebf3d70051777b7fc293b5b2d02df
Niko Heller <acc.hellerniko@gmail.com>
2025-05-02 11:09:29 +0200

    39: support other locales in email previews

--- packages/emails/src/components/email-context.tsx
+++ packages/emails/src/components/email-context.tsx
@@ -3,7 +3,9 @@ import type { EmailContext } from "../types";
 
 i18nInstance.init({
   ...i18nDefaultConfig,
-  initImmediate: true,
+  // In email development mode, i18next translations will only work if translation files
+  // are loaded synchronously. Therefore, this flag must be set to false.
+  initImmediate: false,
 });
 
 export const previewEmailContext: EmailContext = {
@@ -12,6 +14,6 @@ export const previewEmailContext: EmailContext = {
   domain: "rallly.co",
   supportEmail: "support@rallly.co",
   i18n: i18nInstance,
-  t: i18nInstance.getFixedT("en"),
+  t: i18nInstance.t,
   appName: "Rallly",
 };
