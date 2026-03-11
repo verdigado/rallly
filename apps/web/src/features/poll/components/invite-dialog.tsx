506f225d44130870627267361127f83c76a7b506
Giuliano Mele <giuliano.mele@verdigado.com>
2026-03-11 13:54:36 +0100

    29: increase invite dialog size

--- apps/web/src/features/poll/components/invite-dialog.tsx
+++ apps/web/src/features/poll/components/invite-dialog.tsx
@@ -61,7 +61,7 @@ export const InviteDialog = () => {
           <Trans i18nKey="share" defaults="Share" />
         </span>
       </DialogTrigger>
-      <DialogContent data-testid="invite-participant-dialog">
+      <DialogContent size="lg" data-testid="invite-participant-dialog">
         <div className="flex">
           <Share2Icon className="size-6 text-primary" />
         </div>
