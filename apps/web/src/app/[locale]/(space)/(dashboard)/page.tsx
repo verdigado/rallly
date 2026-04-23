c67b29b7037a61181acd3f9d1af45a1cb920ebbc
Giuliano Mele <giuliano.mele@verdigado.com>
2026-04-23 12:51:42 +0200

    Revert "29: add termite alert on dashboard"
    This reverts commit 551692eb2bd9dc5569e2478ad0906d3ea30e7641.

--- apps/web/src/app/[locale]/(space)/(dashboard)/page.tsx
+++ apps/web/src/app/[locale]/(space)/(dashboard)/page.tsx
@@ -27,7 +27,6 @@ import { getTranslation } from "@/i18n/server";
 import { IfFeatureEnabled } from "@/lib/feature-flags/client";
 import { isFeatureEnabled } from "@/lib/feature-flags/server";
 import { PasswordSetupAlert } from "./password-setup-alert";
-import { TermiteAlert } from "./termite-alert";
 
 async function loadData() {
   const [space, user] = await Promise.all([requireSpace(), requireUser()]);
@@ -79,9 +78,6 @@ export default async function Page() {
 
   return (
     <PageContainer>
-      <div className="max-w-4xl">
-        <TermiteAlert />
-      </div>
       <PageHeader>
         <PageTitle>
           <Trans i18nKey="home" defaults="Home" />
